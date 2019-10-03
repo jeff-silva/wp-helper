e236125be14fcb3da64904cefd29312f16a00520
Jeferson Inácio <jeferson.i.silva@gmail.com>
2019-10-03 20:01:25 -0300

    Update elementor-library.php

--- elementor-library.php
+++ elementor-library.php
@@ -33,11 +33,38 @@ class {$class_name} extends \Elementor\Widget_Base {
 		    return [];
 		}
 
-		protected function _register_controls() {}
+		protected function _register_controls() {
+			\$this->start_controls_section('section_heading', [
+				'label' => 'Configurações',
+			]);
+
+			\$this->add_control('script', [
+				'label' => 'Javascript',
+				'type' => \Elementor\Controls_Manager::CODE,
+				'default' => '',
+				'label_block' => true,
+			]);
+
+			\$this->add_control('style', [
+				'label' => 'CSS',
+				'type' => \Elementor\Controls_Manager::CODE,
+				'default' => '',
+				'label_block' => true,
+			]);
+
+			\$this->end_controls_section();
+		}
 
 		protected function render() {
-			\$pluginElementor = \Elementor\Plugin::instance();
-			echo \$pluginElementor->frontend->get_builder_content($post->ID);
+			\$set = (object) \$this->get_settings();
+
+			echo \Elementor\Plugin::\$instance->frontend->get_builder_content($post->ID, true);
+			echo '<script>'. \$set->script .'</script>';
+			echo '<style>'. \$set->style .'</style>';
+
+			if (isset(\$_GET['action']) AND in_array(\$_GET['action'], ['elementor', 'elementor_ajax'])): ?>
+			<style>body.elementor-editor-active .elementor-{$post->ID}:after {content:"Este elemento foi importado da biblioteca do Elementor. Para editá-lo, acesse a opção 'Modelos > {$post->post_title}' do painel"; position:absolute; top:0px; left:0px; width:100%; height:100%; background:#00000044; z-index:99; text-align:center; padding:50px 30% 0 30%; color:#fff;}</style>
+			<?php endif;
 		}
 
 		protected function content_template() {}
